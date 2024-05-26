bbd72b0b9ee899b22a966574da817a565b744ba2
AccessClem <63481051+Clem-Ekeocha@users.noreply.github.com>
2024-05-26 01:31:37 +0100

    Created a new send_email function and completed the program to send email using the API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### The following application is the development of an application using python to extract news information using API
+### The following application is the development of an application 
+### using python to extract news information using API
+### and send as en email alert
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ This set of python codes are used to extract news information using API
 """
 
 import requests as rq
+from send_email import send_email
 
 api_key = "80fc704b938b4540811be9dc10c79508"
+
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-04-25&sortBy=publishedAt&api" \
       "Key=80fc704b938b4540811be9dc10c79508"
 
@@ -15,6 +17,11 @@ request = rq.get(url)
 content = request.json()
 
 #Access the article titles and description
+email_body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        email_body = email_body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+email_body = email_body.encode("utf-8")
+
+send_email(message=email_body)
