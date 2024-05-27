5c7f34ea6d1d5fd8809e3625dfba40b91f5d4ca7
AccessClem <63481051+Clem-Ekeocha@users.noreply.github.com>
2024-05-27 12:11:46 +0100

    Refactored the entire code and added links to the email body, specified language as english in url link, limited the news to 20 and introduced a variable for news category.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### The following application is the development of an application 
-### using python to extract news information using API
-### and send as en email alert
+It is developed using python to extract news information through an API and send as en email alert
+
+Components of the program would include:
+1. An api key
+2. url
+3. customer email send functions
+
+The program would be able to send news alerts on a daily.
--- main.py
+++ main.py
@@ -5,10 +5,20 @@ This set of python codes are used to extract news information using API
 import requests as rq
 from send_email import send_email
 
+# Create a variable called topic to decide what particlaur topics to extract and avoid tampering with the url.
+# The topic is set to tesla for this test run.
+topic = "tesla"
+
 api_key = "80fc704b938b4540811be9dc10c79508"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-04-25&sortBy=publishedAt&api" \
-      "Key=80fc704b938b4540811be9dc10c79508"
+# Added "&language=en" at the end of the url to extract only news in english language
+# Adjusted the 2nd line with f block to make the extracted topic dynamic
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \   
+      "from=2024-04-27" \
+      "&sortBy=publishedAt&" \
+      "apiKey=80fc704b938b4540811be9dc10c79508&" \
+      "language=en"
 
 #Make a request
 request = rq.get(url)
@@ -16,12 +26,24 @@ request = rq.get(url)
 # Got a dictionary
 content = request.json()
 
-#Access the article titles and description
+# Access the article titles, description, and links to read further
 email_body = ""
-for article in content["articles"]:
-    if article["title"] is not None:
-        email_body = email_body + article["title"] + "\n" + article["description"] + 2*"\n"
 
+# Iterate to pick only the first 20 emails
+for article in content["articles"][:20]:
+
+    # Skip the article if the title is None
+    if article["title"] is None:
+        continue
+
+    subject = "Subject: Today's News"
+    title = article["title"]
+    description = article["description"] if article["description"] else ""
+    email_body += f"{subject}\n{title}\n{description}\n{article['url']}\n\n"
+
+# Convert the email body to encode the message into email format
 email_body = email_body.encode("utf-8")
 
 send_email(message=email_body)
+
+
